6e4e05aeb5a6cc90758fdceaba69ae0a99a4a9af
Gustavo Guirele Avelar <gustavo.guirele@gmail.com>
2026-01-15 16:46:08 -0300

    Initialize user lists and update main display logic

--- App.java
+++ App.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args){
-        new TelaPrincipal().setVisible(true);
+        ArrayList<String> listaUsuarios = new ArrayList<>();
+
+        TelaPrincipal telaCadastro = new TelaPrincipal(listaUsuarios);
+        TelaUsuarios telaBanco = new TelaUsuarios(listaUsuarios);
+
+        telaCadastro.setVisible(true);
+        telaBanco.setVisible(true);
+
     }
 }
